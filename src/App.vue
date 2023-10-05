27cc63cb2b9a69b35f90eb0ce62c8748abb6743c
rickCrz7 <cruzr@cslab-08.ad.cs.southwesternadventist.edu>
2023-10-05 15:02:18 -0500

    first commit

---
+++ src/App.vue
@@ -0,0 +1,7 @@
+<template>
+    <div
+        class="container mx-auto max-w-prose p-2 min-h-screen flex items-center justify-center"
+    >
+        <h1 class="text-5xl tracking-widest">TODOS</h1>
+    </div>
+</template>
